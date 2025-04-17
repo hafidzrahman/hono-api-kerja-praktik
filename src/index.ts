1203935dab9c9663ea186128bc121c04b89ceb7e
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-17 17:16:50 +0700

    🔥 feat: initialize Hono server with middleware and routing setup

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { cors } from "hono/cors";
+import { RegExpRouter } from "hono/router/reg-exp-router";
+import { BlankEnv, BlankSchema } from "hono/types";
+import GlobalHandler from "./handlers/global.handler";
+import globalRoute from "./routes/global.route";
+import bimbinganKPRoute from "./routes/bimbingan-kp.route";
+import dailyReportRoute from "./routes/daily-report.route";
+import seminarKPRoute from "./routes/seminar-kp.route";
+import daftarKPRoute from "./routes/daftar-kp.route";
+
+// Init Hono Object and Load environment variables from .env file
+const app: Hono<BlankEnv, BlankSchema, "/"> = new Hono({ router: new RegExpRouter() });
+const { APP_PORT }: NodeJS.ProcessEnv = process.env;
+
+// Load all available middlewares
+app.use('*', logger())
+app.use('*', cors({
+  origin: '*', // restrict this in production
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+  exposeHeaders: ['Content-Length', 'X-Request-Id'],
+  maxAge: 3600,
+  credentials: true
+}))
+
+// Load all default routes for common handling
+app.notFound(GlobalHandler.notFound);
+app.onError(GlobalHandler.error);
+
+// Load all available routes
+app.route("/", globalRoute);
+app.route("/daftar-kp", daftarKPRoute);
+app.route("/daily-report", dailyReportRoute);
+app.route("/bimbingan-kp", bimbinganKPRoute);
+app.route("/seminar-kp", seminarKPRoute);
+
+export default {
+  port: APP_PORT || 5000,
+	fetch: app.fetch,
+};
+
+console.log(`[INFO] Server is on fire at port ${APP_PORT}! 🔥`);
