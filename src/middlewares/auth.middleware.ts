188cef21015fbbaeb1801a1c6da3ad2cb4543bcb
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-17 17:17:49 +0700

    feat: implement AuthMiddleware with JWT bearer token extraction

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,23 @@
+import { Context, Next } from "hono";
+import AuthHelper from "../helpers/auth.helper";
+import { APIError } from "../utils/api-error.util";
+
+export default class AuthMiddleware {
+	public static async JWTBearerTokenExtraction(c: Context, next: Next) {
+		const authHeader = c.req.header("Authorization");
+
+		if (!authHeader || !authHeader.startsWith("Bearer ")) {
+			throw new APIError("Hadeh, format authorization header-nya salah mas! 😡", 401);
+		}
+
+		const token = authHeader.split(" ")[1];
+
+		try {
+			const payload = AuthHelper.decodeJwtPayload(token);
+			c.set("user", payload);
+			await next();
+		} catch (error) {
+			throw new APIError("Waduh, token-nya salah mas, kagak valid! 😡", 401);
+		}
+	}
+}
