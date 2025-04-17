bfd114bce7326c85191b4328ba967db6b8918366
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-17 17:17:24 +0700

    🔥 feat: implement GlobalHandler with introduce, health, notFound, and error methods

---
+++ src/handlers/global.handler.ts
@@ -0,0 +1,38 @@
+import { Context } from "hono";
+import { BlankEnv, BlankInput, HTTPResponseError } from "hono/types";
+import GlobalService from "../services/global.service";
+import { GlobalServiceHealthResponse, GlobalServiceIntroduceResponse } from "../types/global.type";
+
+export default class GlobalHandler {
+
+    public static async introduce(c: Context<BlankEnv, "/", BlankInput>) {
+        const introduceMessage: GlobalServiceIntroduceResponse = await GlobalService.introduce();
+        return c.json(introduceMessage, 200);
+    }
+
+    public static async health(c: Context<BlankEnv, "/", BlankInput>) {
+        const healthMessage: GlobalServiceHealthResponse = await GlobalService.health();
+        return c.json(healthMessage, 200);
+    }
+
+    public static async notFound(c: Context<BlankEnv, any, {}>) {
+        return c.json(
+            {
+                response: false,
+                message: "Waduh, mau nyari apa kamu mas? 😅",
+            },
+            404
+        );
+    }
+    
+    public static async error(err: Error | HTTPResponseError | any, c: Context<BlankEnv, any, {}>) {        
+        return c.json(
+            {
+                response: false,
+                message: err.message,
+            },
+            err.statusCode || 500
+        );
+    }
+
+}
