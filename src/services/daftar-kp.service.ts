8f34a822f81f339e299d869ca4cba699b2f017c1
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-04-19 08:18:13 +0700

    feat: refactor createPermohonan method to use structured parameters and update response message

--- src/services/daftar-kp.service.ts
+++ src/services/daftar-kp.service.ts
@@ -1,21 +1,30 @@
 import MahasiswaHelper from "../helpers/mahasiswa.helper";
+import DaftarKPRepository from "../repositories/daftar-kp.repository";
 import MahasiswaRepository from "../repositories/mahasiswa.repository";
-import { MahasiswaServiceCreatePermohonanResponse } from "../types/mahasiswa.type";
+import { CreatePermohonanRequestInterface, CreatePermohonanResponseInterface } from "../types/daftar-kp/service.type";
 import { APIError } from "../utils/api-error.util";
 
 export default class DaftarKPService {
 
-    public static async createPermohonan(email: string, tanggalTerdaftar: Date, idInstansi: string, tujuanSuratInstansi: string): Promise<MahasiswaServiceCreatePermohonanResponse> {
-        const infoDasarMahasiswa = await MahasiswaRepository.findByEmail(email);
+    public static async createPermohonan({ email, tanggalMulai, idInstansi, tujuanSuratInstansi }: CreatePermohonanRequestInterface): Promise<CreatePermohonanResponseInterface> {
+
+        const infoDasarMahasiswa = await MahasiswaRepository.findByEmail({email});
         if (!infoDasarMahasiswa) {
             throw new APIError('Mahasiswa tidak ditemukan! 😭', 404);
         }
         const { nim } = infoDasarMahasiswa;
         const semester = MahasiswaHelper.getSemesterByNIM(nim);
 
+        // logika bisnis lainnya buat disini lengkap detail, sampe mhs bisa daftar kp
+
+        const pendaftaranKP = await DaftarKPRepository.createPermohonan({
+            nim: nim,
+            semester: semester,
+        });
+
         return {
             response: true,
-            message: 'Berikut info dosen lengkap serta detail mahasiswa per angkatan (max 8 akt)! 😁',            
+            message: 'Yeay, Permohonan KP berhasil dibuat! 😁',            
         };
     }
     
