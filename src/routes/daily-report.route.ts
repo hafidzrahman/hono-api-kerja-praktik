582d9af37caf06fe2251c88f3121a2b193a9ec80
rel1vee <muhzakierbaisyas@gmail.com>
2025-04-20 17:43:29 +0700

    feat: update daily report route to include CRUD operations and access checks

--- src/routes/daily-report.route.ts
+++ src/routes/daily-report.route.ts
@@ -1,10 +1,23 @@
 import { Hono } from "hono";
 import { RegExpRouter } from "hono/router/reg-exp-router";
-import DaftarKPHandler from "../handlers/daftar-kp.handler";
+import DailyReportHandler from "../handlers/daily-report.handler";
 import AuthMiddleware from "../middlewares/auth.middleware";
 
 const dailyReportRoute = new Hono({ router: new RegExpRouter() });
 
-dailyReportRoute.get("/", AuthMiddleware.JWTBearerTokenExtraction, DaftarKPHandler.createPermohonan);
+dailyReportRoute.use(AuthMiddleware.JWTBearerTokenExtraction);
+
+dailyReportRoute.get("/check-access", DailyReportHandler.checkAccessLevel);
+dailyReportRoute.get("/", DailyReportHandler.getDailyReport);
+dailyReportRoute.post("/", DailyReportHandler.createDailyReport);
+dailyReportRoute.post("/detail", DailyReportHandler.createDetailDailyReport);
+dailyReportRoute.put("/detail", DailyReportHandler.updateDetailDailyReport);
+dailyReportRoute.get("/nilai", DailyReportHandler.getNilaiForMahasiswa);
+
+dailyReportRoute.put("/", DailyReportHandler.evaluateDailyReport);
+dailyReportRoute.get("/mahasiswa-instansi", DailyReportHandler.getMahasiswaAndDailyReportForPembimbing);
+dailyReportRoute.post("/nilai", DailyReportHandler.createNilaiForMahasiswa);
+
+dailyReportRoute.get("/mahasiswa-bimbingan", DailyReportHandler.getMahasiswaAndDailyReportForDosen);
 
 export default dailyReportRoute;
