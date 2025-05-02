98993c6dd77d4f574b201b9d86895493cdf3d12d
Hafidz Alhadid Rahman <hafidzrahman559@gmail.com>
2025-05-02 15:40:47 +0700

    route daftar kp

--- src/types/daftar-kp/service.type.ts
+++ src/types/daftar-kp/service.type.ts
@@ -1,12 +1,36 @@
 import { CommonResponse } from "../global.type";
+import { jenis_instansi, pendaftaran_kp } from "../../generated/prisma";
+import { RepositoryRiwayatPendaftaranKPInterface } from "./repository.type";
 
-export interface CreatePermohonanRequestInterface {
-    email: string;
-	tanggalMulai: Date;
-	idInstansi: string;
-	tujuanSuratInstansi: string;
+export interface CreatePermohonanPendaftaranKPInterface {
+    email : string,
+    tanggalMulai : Date,
+    idInstansi : string,
+    tujuanSuratInstansi : string
 }
 
-export interface CreatePermohonanResponseInterface extends CommonResponse {
+export interface CreatePermohonanPendaftaranInstansiInterface {
+    email : string,
+    namaInstansi : string,
+    alamatInstansi : string,
+    jenisInstansi : jenis_instansi,
+    profilSingkat? : string
+    longitude? : number,
+    latitude? : number,
+    namaPenanggungJawabInstansi : string,
+    telpPenanggungJawabInstansi : string
+}
 
+export interface GetRiwayatPendaftaranKP extends CommonResponse {
+    data : RepositoryRiwayatPendaftaranKPInterface[] | null
+}
+
+export interface GetBerkasMahasiswa extends CommonResponse {
+    data : {
+        suratPengantar : pendaftaran_kp[] | null,
+        suratBalasan : pendaftaran_kp[] | null,
+        idPengajuanDosenPembimbing : pendaftaran_kp[] | null,
+        suratPenunjukkanDosenPembimbing : pendaftaran_kp[] | null,
+        suratPerpanjanganKP : pendaftaran_kp[] | null
+    }
 }
