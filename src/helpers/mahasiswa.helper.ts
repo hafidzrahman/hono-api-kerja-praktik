7c2fcffab5f30cfc6450b7e1e59a74fe5d952c5f
Gindra-o7 <gilangindra734@gmail.com>
2025-05-08 19:29:51 +0700

    ✨ feat: update getSemesterByNIM method to improve semester calculation logic

--- src/helpers/mahasiswa.helper.ts
+++ src/helpers/mahasiswa.helper.ts
@@ -1,10 +1,15 @@
 export default class MahasiswaHelper {
-	// Static helper untuk decode JWT payload
 	public static getSemesterByNIM(nim: string): number {
-        const angkatan = nim.substring(0, 2);
-        const tahunSekarang = new Date().getFullYear().toString().substring(2, 4);
-        let semester = (Number(angkatan) - Number(tahunSekarang)) * 2;
-        if (new Date().getMonth() >= 7) {
+        const angkatan = nim.substring(1, 3);
+        const tahunMasuk = 2000 + Number(angkatan)
+
+        const tahunSekarang = new Date().getFullYear();
+        const bulanSekarang = new Date().getMonth();
+
+        let tahunLewat = tahunSekarang - tahunMasuk;
+        let semester = tahunLewat * 2;
+
+        if (bulanSekarang >= 7) {
             semester += 1;
         }
         return semester;
