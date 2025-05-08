2c722ede90744e549540f089010bbbe8be54b505
Gindra-o7 <gilangindra734@gmail.com>
2025-05-08 11:13:25 +0700

    ✨ feat: extend DokumenSeminarKP types with additional interfaces for PendaftaranKP, Jadwal, and Nilai

--- src/types/seminar-kp/dokumen-seminar-kp.type.ts
+++ src/types/seminar-kp/dokumen-seminar-kp.type.ts
@@ -1,4 +1,4 @@
-import { jenis_dokumen, status_dokumen } from "../../generated/prisma";
+import { jenis_dokumen, status_dokumen, status_jadwal } from "../../generated/prisma";
 
 export interface DokumenSeminarKp {
   id: string;
@@ -22,10 +22,49 @@ export interface UpdateDokumenSeminarKPInput {
   status?: status_dokumen;
   komentar?: string;
 }
+interface Instansi {
+  nama: string;
+  alamat: string;
+}
+interface Dosen {
+  nip: string;
+  nama: string;
+  no_hp: string | null;
+}
+interface PendaftaranKP {
+  id: string;
+  judul_kp: string | null;
+  id_instansi: string | null;
+  tanggal_mulai: Date;
+  tanggal_selesai: Date | null;
+  instansi: Instansi | null;
+  dosen_pembimbing: Dosen | null;
+  dosen_penguji: Dosen | null;
+}
+
+interface Ruangan {
+  nama: string;
+}
+interface Jadwal {
+  id: string | null;
+  tanggal: Date | null;
+  waktu_mulai: Date | null;
+  waktu_selesai: Date | null;
+  status: status_jadwal | null;
+  ruangan: Ruangan | null;
+}
+
+interface Nilai {
+  nilai_penguji: number | null;
+  nilai_instansi: number | null;
+  nilai_pembimbing: number | null;
+  nilai_akhir: number | null;
+}
+
 export interface DokumenSeminarKP {
-  id: string,
-  jenis_dokumen: jenis_dokumen,
-  link_path: string,
+  id: string;
+  jenis_dokumen: jenis_dokumen;
+  link_path: string;
   tanggal_upload: Date | null;
   status: status_dokumen | null;
   komentar: string | null;
@@ -36,5 +75,15 @@ export interface MahasiswaWithDokumen {
   nim: string;
   nama: string;
   email: string;
-  dokumen_seminar_kp: DokumenSeminarKP[]
+  dokumen_seminar_kp: DokumenSeminarKP[];
+}
+
+export interface SeminarKPSaya {
+  nim: string;
+  nama: string;
+  email: string;
+  pendaftaran_kp: PendaftaranKP[];
+  dokumen_seminar_kp: DokumenSeminarKP[];
+  jadwal: Jadwal[];
+  nilai: Nilai[];
 }
