86fc5fc9d1f6ec77564964cfc9e43ea46c287513
Gindra-o7 <gilangindra734@gmail.com>
2025-05-13 22:58:12 +0700

    ⚙refactor: expand nilai type definitions for comprehensive grading components and responses

--- src/types/seminar-kp/nilai.type.ts
+++ src/types/seminar-kp/nilai.type.ts
@@ -1,3 +1,63 @@
+import { jenis_dokumen, status_dokumen, status_pendaftaran } from "../../generated/prisma";
+
+export enum StatusNilai {
+  NILAI_BELUM_VALID = 'Nilai Belum Valid',
+  NILAI_VALID = 'Nilai Valid',
+  NILAI_APPROVE = 'Nilai Approve'
+}
+
+export interface KomponenNilaiInstansi {
+  deliverables?: number;
+  ketepatan_waktu?: number;
+  kedisiplinan?: number;
+  attitude?: number;
+  kerjasama_tim?: number;
+  inisiatif?: number;
+  masukan?: string;
+}
+
+export interface KomponenNilaiPembimbing {
+  penyelesaian_masalah?: number;
+  bimbingan_sikap?: number;
+  kualitas_laporan?: number;
+  catatan?: string;
+}
+
+export interface KomponenNilaiPenguji {
+  penguasaan_keilmuan?: number;
+  kemampuan_presentasi?: number;
+  kesesuaian_urgensi?: number;
+  catatan?: string;
+}
+
+export interface DetailMahasiswaNilai {
+  nim: string;
+  nama: string;
+  kelas?: string;
+  statusNilai: StatusNilai;
+  statusDaftarKp?: status_pendaftaran;
+  semester?: string;
+  instansi?: string;
+  pembimbingInstansi?: string;
+  dosenPembimbing?: string;
+  dosenPenguji?: string;
+  nilaiInstansi?: number;
+  nilaiPembimbing?: number;
+  nilaiPenguji?: number;
+  nilaiAkhir?: number;
+  komponenNilaiInstansi?: KomponenNilaiInstansi;
+  komponenNilaiPembimbing?: KomponenNilaiPembimbing;
+  komponenNilaiPenguji?: KomponenNilaiPenguji;
+}
+
+export interface AllNilaiResponse {
+  tahunAjaran: string;
+  jumlahNilaiBelumValid: number;
+  jumlahNilaiValid: number;
+  jumlahNilaiApprove: number;
+  detailMahasiswa: DetailMahasiswaNilai[];
+}
+
 export interface NilaiPengujiInput {
   penguasaanKeilmuan: number;
   kemampuanPresentasi: number;
