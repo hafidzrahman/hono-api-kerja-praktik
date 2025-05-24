e80bb240fe5caa3f905c897d14c93a0968c8923f
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-05-24 14:47:28 +0700

    ⚙refactor: remove obsolete migration files and drop unnecessary columns from database schema

--- prisma/migrations/20250524003229_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `radius` to the `instansi` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "instansi" ADD COLUMN     "radius" DOUBLE PRECISION NOT NULL;
--- prisma/migrations/20250524004801_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "instansi" ALTER COLUMN "radius" DROP NOT NULL;
