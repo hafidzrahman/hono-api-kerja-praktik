a47a3e382041098d9d105f86af585ac37fdc2142
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-07-02 16:19:02 +0700

    ⚡feat: add kriteria_pendaftaran_kp seeding to database initialization

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,18 +4,53 @@ console.log("[INFO] Seeding database...");
 
 async function main() {
 	console.log("[DEBUG] Running createMany...");
-	const result = await prisma.ruangan.createMany({
+	
+	const resultKriteria = await prisma.kriteria_pendaftaran_kp.createMany({
+		data: [
+			{
+				id: 0,
+				nama: "surat penolakan instansi",
+			},
+			{
+				id: 1,
+				nama: "surat pengantar instansi",
+			},
+			{ id: 2, nama: "surat balasan dari instansi" },
+			{ id: 3, nama: "id penunjukkan dosen pembimbing" },
+			{ id: 4, nama: "surat penunjukkan dosen pembimbing" },
+			{
+				id: 5,
+				nama: "surat perpanjangan kerja praktik",
+			},
+		],
+		skipDuplicates: true,
+	});
+	
+	console.log(
+		"[DEBUG] Result of inserted ruangan createMany:",
+		resultKriteria.count > 0
+			? resultKriteria
+			: "Data was inserted previously, no new data inserted."
+	);
+
+	const resultRuangan = await prisma.ruangan.createMany({
 		data: [
 			{ nama: "FST-301" },
 			{ nama: "FST-302" },
 			{ nama: "FST-303" },
 			{ nama: "FST-304" },
 			{ nama: "FST-305" },
+			{ nama: "FST-306" },
 		],
 		skipDuplicates: true,
 	});
 
-	console.log("[DEBUG] Result of inserted ruangan createMany:", result.count > 0 ? result : "Data was inserted previously, no new data inserted.");
+	console.log(
+		"[DEBUG] Result of inserted ruangan createMany:",
+		resultRuangan.count > 0
+			? resultRuangan
+			: "Data was inserted previously, no new data inserted."
+	);
 }
 
 main()
